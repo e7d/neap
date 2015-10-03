3d3c9cdcb7d2597d153d391b8f3680867980f6ca
e7d <mail@e7d.io>
2015-10-03 07:45:16 +0200

    added default local configuration added Ping service

--- api/module/Status/config/module.config.php
+++ api/module/Status/config/module.config.php
@@ -3,6 +3,7 @@ return array(
     'controllers' => array(
         'factories' => array(
             'Status\\V1\\Rpc\\Version\\Controller' => 'Status\\V1\\Rpc\\Version\\VersionControllerFactory',
+            'Status\\V1\\Rpc\\Ping\\Controller' => 'Status\\V1\\Rpc\\Ping\\PingControllerFactory',
         ),
     ),
     'router' => array(
@@ -17,11 +18,22 @@ return array(
                     ),
                 ),
             ),
+            'status.rpc.ping' => array(
+                'type' => 'Segment',
+                'options' => array(
+                    'route' => '/api/ping',
+                    'defaults' => array(
+                        'controller' => 'Status\\V1\\Rpc\\Ping\\Controller',
+                        'action' => 'ping',
+                    ),
+                ),
+            ),
         ),
     ),
     'zf-versioning' => array(
         'uri' => array(
             0 => 'status.rpc.version',
+            1 => 'status.rpc.ping',
         ),
     ),
     'zf-rpc' => array(
@@ -32,13 +44,26 @@ return array(
             ),
             'route_name' => 'status.rpc.version',
         ),
+        'Status\\V1\\Rpc\\Ping\\Controller' => array(
+            'service_name' => 'Ping',
+            'http_methods' => array(
+                0 => 'GET',
+            ),
+            'route_name' => 'status.rpc.ping',
+        ),
     ),
     'zf-content-negotiation' => array(
         'controllers' => array(
             'Status\\V1\\Rpc\\Version\\Controller' => 'Json',
+            'Status\\V1\\Rpc\\Ping\\Controller' => 'Json',
         ),
         'accept_whitelist' => array(
             'Status\\V1\\Rpc\\Version\\Controller' => array(
+                0 => 'application/vnd.status.v1+json',
+                1 => 'application/json',
+                2 => 'application/*+json',
+            ),
+            'Status\\V1\\Rpc\\Ping\\Controller' => array(
                 0 => 'application/vnd.status.v1+json',
                 1 => 'application/json',
                 2 => 'application/*+json',
@@ -48,6 +73,26 @@ return array(
             'Status\\V1\\Rpc\\Version\\Controller' => array(
                 0 => 'application/vnd.status.v1+json',
                 1 => 'application/json',
+            ),
+            'Status\\V1\\Rpc\\Ping\\Controller' => array(
+                0 => 'application/vnd.status.v1+json',
+                1 => 'application/json',
+            ),
+        ),
+    ),
+    'zf-content-validation' => array(
+        'Status\\V1\\Rpc\\Ping\\Controller' => array(
+            'input_filter' => 'Status\\V1\\Rpc\\Ping\\Validator',
+        ),
+    ),
+    'input_filter_specs' => array(
+        'Status\\V1\\Rpc\\Ping\\Validator' => array(
+            0 => array(
+                'required' => true,
+                'validators' => array(),
+                'filters' => array(),
+                'name' => 'ack',
+                'description' => 'Acknowledge the request with a timestamp',
             ),
         ),
     ),

32a7c981bca017894234e7a16763c47f423f284b
e7d <mail@e7d.io>
2015-11-07 15:13:50 +0100

    updated config

--- config/generate-certificates.sh
+++ config/generate-certificates.sh
@@ -2,6 +2,10 @@
 
 SRC=/usr/src
 
+# ToDo: Look inside /resources/ssl for provided certificates
+# echo "Find for provided certificates"
+
+# ToDo: Check for a valid reverse hostname to generate let's encrypt certificates
 # echo "Generate \"Let's Encrypt\" certificates"
 # read -p "Domain name: " domain
 # if [ ! -z "$domain" ]
@@ -15,13 +19,11 @@ SRC=/usr/src
 #     ./venv/bin/letsencrypt --agree-eula --agree-tos --authenticator standalone -d $domain auth
 # fi
 
+# As a backup, generate untrusted local certifiactes
 echo "Generate default certificates"
 mkdir -p /etc/ssl/localcerts
 openssl req -new -x509 -subj "/CN=`uname -n`/O=Neap/C=US" -days 3650 -nodes -out /etc/ssl/localcerts/self.pem -keyout /etc/ssl/localcerts/self.key
 chmod -cR 600 /etc/ssl/localcerts/self.*
-
-# ToDo: look inside /resources/ssl for provided certificates
-# echo "Find for provided certificates"
 
 echo "Generate dhparam file"
 openssl dhparam -out /etc/ssl/private/dhparam.pem 2048

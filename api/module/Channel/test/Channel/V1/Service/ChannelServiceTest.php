82f7b71f5adece69f29c72f1398583d5c860e6a5
e7d <mail@e7d.io>
2015-12-04 12:13:11 +0100

    test conf travis

---
+++ api/module/Channel/test/Channel/V1/Service/ChannelServiceTest.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Neap (http://neap.io/)
+ *
+ * @link      http://github.com/e7d/neap for the canonical source repository
+ * @copyright Copyright (c) 2015 Michaël "e7d" Ferrand (http://github.com/e7d)
+ * @license   https://github.com/e7d/neap/blob/master/LICENSE.md The MIT License
+ */
+
+namespace Channel\V1\Service;
+
+use Zend\Test\PHPUnit\Controller\AbstractHttpControllerTestCase;
+
+class ChannelServiceTest extends AbstractHttpControllerTestCase
+{
+    private $serviceManager;
+
+    public function setUp()
+    {
+        $this->setApplicationConfig(
+            include './config/application.config.php'
+        );
+        parent::setUp();
+        $this->serviceManager = $this->getApplicationServiceLocator();
+        $this->serviceManager->setAllowOverride(true);
+    }
+
+    public function getServiceManager()
+    {
+        return $this->serviceManager;
+    }
+
+    public function testClassType()
+    {
+        $channelService = $this->serviceManager->get('Channel\V1\Service\ChannelService');
+        $this->assertEquals('Channel\V1\Service\ChannelService', get_class($channelService));
+    }
+}

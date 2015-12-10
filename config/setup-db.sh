4847423c0179f96a0f0084865ff6fd6383eb6486
e7d <mail@e7d.io>
2015-12-10 17:29:04 +0100

    fixed indexes genreation problem

--- config/setup-db.sh
+++ config/setup-db.sh
@@ -6,6 +6,8 @@ echo "Install PostgreSQL"
 apt-get -y install postgresql postgresql-contrib
 
 echo "Create and populate API database"
+# Disconnect every remaining connection
+sudo -u "postgres" psql "postgres"  -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = 'neap'";
 sudo -u "postgres" psql "postgres" < ${DIR}/../resources/database/init.sql;
 sudo -u "postgres" psql "neap" < ${DIR}/../resources/database/structure.sql;
 sudo -u "postgres" psql "neap" < ${DIR}/../resources/database/oauth2.sql;

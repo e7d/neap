9ba65bd3180336dbe81a4bc796ea3347332191cd
e7d <mail@e7d.io>
2015-12-11 10:28:08 +0100

    improve api module test script

--- api/test.sh
+++ api/test.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 
 DIR=`dirname $0`
-cd $DIR
-ls
+CYAN='\033[4;36m'
+DEFAULT='\033[0m'
 
-echo "Update composer"
+echo "${CYAN}Enter $DIR directory${DEFAULT}"
+cd $DIR
+
+echo "${CYAN}Update composer${DEFAULT}"
 composer self-update
 composer install --no-interaction --ignore-platform-reqs --prefer-source
 
-echo "Copy latest configuration files"
+echo "${CYAN}Copy latest configuration files${DEFAULT}"
 cp config/autoload/local.php.dist config/autoload/local.php
 cp config/autoload/oauth2.local.php.dist config/autoload/oauth2.local.php
 
-echo "Run phpunit tests"
+echo "${CYAN}Run phpunit tests${DEFAULT}"
 phpunit -c module/Channel/test/phpunit.xml

a02ec63afca1fbc0d52682d8ac87d5bfbf71b68c
e7d <mail@e7d.io>
2015-12-22 15:10:24 +0100

    fix api setup to ensure we do not get any unwanted cache

--- bootstrap/setup-api.sh
+++ bootstrap/setup-api.sh
@@ -8,14 +8,17 @@ try
     cd ${DIR}/api/
     composer install --no-interaction --prefer-source
 
-    echo "Enable development mode"
-    sleep 1s
-    php ${DIR}/api/public/index.php development enable
-
     echo "Copy Neap configuration files"
     cp ${DIR}/api/config/development.config.php.dist ${DIR}/api/config/development.config.php
     cp ${DIR}/api/config/autoload/local.php.dist ${DIR}/api/config/autoload/local.php
     cp ${DIR}/api/config/autoload/oauth2.local.php.dist ${DIR}/api/config/autoload/oauth2.local.php
+
+    echo "Remove cache files"
+    rm -rf ${DIR}/api/data/cache/*.cache.php
+
+    echo "Enable development mode"
+    sleep 1s
+    php ${DIR}/api/public/index.php development enable
 )
 catch || {
     case $ex_code in

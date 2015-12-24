18ff5e576399113ec7c84e8620ffcc8dff8bfd69
e7d <mail@e7d.io>
2015-12-24 17:42:20 +0100

    updated composer dependency file added code coverage test added code quality test

--- api/test.sh
+++ api/test.sh
@@ -15,21 +15,10 @@ cp -v config/autoload/local.php.dist config/autoload/local.php
 cp -v config/autoload/oauth2.local.php.dist config/autoload/oauth2.local.php
 
 echox "${text_cyan}Run phpunit tests"
-./vendor/bin/phpunit -c module/phpunit.xml
-# ./vendor/bin/phpunit -c module/Application/test/phpunit.xml
-# ./vendor/bin/phpunit -c module/Channel/test/phpunit.xml
-# ./vendor/bin/phpunit -c module/Chat/test/phpunit.xml
-# ./vendor/bin/phpunit -c module/Console/test/phpunit.xml
-# ./vendor/bin/phpunit -c module/Emoji/test/phpunit.xml
-# ./vendor/bin/phpunit -c module/Ingest/test/phpunit.xml
-# ./vendor/bin/phpunit -c module/Outage/test/phpunit.xml
-# ./vendor/bin/phpunit -c module/Panel/test/phpunit.xml
-# ./vendor/bin/phpunit -c module/Root/test/phpunit.xml
-# ./vendor/bin/phpunit -c module/RTMP/test/phpunit.xml
-# ./vendor/bin/phpunit -c module/Search/test/phpunit.xml
-# ./vendor/bin/phpunit -c module/Status/test/phpunit.xml
-# ./vendor/bin/phpunit -c module/Stream/test/phpunit.xml
-# ./vendor/bin/phpunit -c module/Team/test/phpunit.xml
-# ./vendor/bin/phpunit -c module/Topic/test/phpunit.xml
-# ./vendor/bin/phpunit -c module/User/test/phpunit.xml
-# ./vendor/bin/phpunit -c module/Video/test/phpunit.xml
+./vendor/bin/phpunit -c module/phpunit.xml --coverage-clover ./build/logs/clover.xml
+
+echox "${text_cyan}Check code quality"
+./vendor/bin/phpcs --standard=PSR2 module/
+
+echox "${text_cyan}Compute code coverage"
+./vendor/bin/coveralls -v

33b2a37c93c5d68ba0b11baf1172920ec71fba38
e7d <mail@e7d.io>
2016-01-06 15:31:53 +0100

    update test runner script to introduce some options

--- api/run-tests.sh
+++ api/run-tests.sh
@@ -14,14 +14,32 @@ echox "${text_cyan}Copy latest configuration files"
 cp -v config/autoload/local.php.dist config/autoload/local.php
 cp -v config/autoload/oauth2.local.php.dist config/autoload/oauth2.local.php
 
-echox "${text_cyan}Run phpunit tests"
-./vendor/bin/phpunit -c module/phpunit.xml --coverage-clover ./build/logs/clover.xml
+if [[ $@ == *"--travis"* ]]; then
+	set @=$@"--coverage-clover --code-quality --coveralls --scrutinizer"
+fi
 
-echox "${text_cyan}Check code quality"
-./vendor/bin/phpcs --standard=PSR2 module/
+if [[ $@ == *"--coverage-clover"* ]]; then
+	echox "${text_cyan}Run phpunit tests with Clover code coverage"
+	./vendor/bin/phpunit -c module/phpunit.xml --coverage-clover ./build/logs/coverage.xml
+elif [[ $@ == *"--coverage-html"* ]]; then
+	echox "${text_cyan}Run phpunit tests with HTML code coverage"
+	./vendor/bin/phpunit -c module/phpunit.xml --coverage-html ./build/logs/coverage
+else
+	echox "${text_cyan}Run phpunit tests"
+	./vendor/bin/phpunit -c module/phpunit.xml
+fi
 
-echox "${text_cyan}Send clover log to Coveralls"
-./vendor/bin/coveralls -v
+if [[ $@ == *"--code-quality"* ]]; then
+	echox "${text_cyan}Check code quality"
+	./vendor/bin/phpcs --standard=PSR2 module/
+fi
 
-echox "${text_cyan}Send clover log to Scrutinizer"
-./vendor/bin/ocular code-coverage:upload --format=php-clover ./build/logs/clover.xml
+if [[ $@ == *"--coveralls"* ]]; then
+	echox "${text_cyan}Send clover log to Coveralls"
+	./vendor/bin/coveralls -v
+fi
+
+if [[ $@ == *"--scrutinizer"* ]]; then
+	echox "${text_cyan}Send clover log to Scrutinizer"
+	./vendor/bin/ocular code-coverage:upload --format=php-clover ./build/logs/coverage.xml
+fi

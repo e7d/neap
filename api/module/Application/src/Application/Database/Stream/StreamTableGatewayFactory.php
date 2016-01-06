bfd59517bd60b0ecb7596b9e6f8cbebe55b2fadb
e7d <mail@e7d.io>
2016-01-06 16:30:05 +0100

    fixed LICENSE file reference uri

--- api/module/Application/src/Application/Database/Stream/StreamTableGatewayFactory.php
+++ api/module/Application/src/Application/Database/Stream/StreamTableGatewayFactory.php
@@ -4,7 +4,7 @@
  *
  * @link      http://github.com/e7d/neap for the canonical source repository
  * @copyright Copyright (c) 2015 Michaël "e7d" Ferrand (http://github.com/e7d)
- * @license   https://github.com/e7d/neap/blob/master/LICENSE.md The MIT License
+ * @license   https://github.com/e7d/neap/blob/master/LICENSE.txt The MIT License
  */
 
 namespace Application\Database\Stream;

0862d623dce18f5c13a548a511302fe67e557e4d
e7d <mail@e7d.io>
2016-01-09 08:38:41 +0100

    replaced copyrights in headers

--- api/module/Search/src/Search/V1/Rest/Topic/TopicCollection.php
+++ api/module/Search/src/Search/V1/Rest/Topic/TopicCollection.php
@@ -3,7 +3,7 @@
  * Neap (http://neap.io/)
  *
  * @link      http://github.com/e7d/neap for the canonical source repository
- * @copyright Copyright (c) 2015 Michaël "e7d" Ferrand (http://github.com/e7d)
+ * @copyright Copyright (c) 2016 Michaël "e7d" Ferrand (http://github.com/e7d)
  * @license   https://github.com/e7d/neap/blob/master/LICENSE.txt The MIT License
  */
 

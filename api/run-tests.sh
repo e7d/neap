18a5da5ddcfa4d925eede7cbd0713c7d0abb2862
e7d <mail@e7d.io>
2016-01-10 13:24:58 +0100

    updated tests script

--- api/run-tests.sh
+++ api/run-tests.sh
@@ -6,26 +6,87 @@ DIR=$(dirname `which $0`)
 
 cd ${DIR}
 
-if [[ $@ == *"--travis"* ]]; then
-	set @=$@"--composer-update --copy-configuration --coverage-clover --code-quality --coveralls --scrutinizer"
+for arg in "$@"
+do
+case $arg in
+	-c|--copy-configuration)
+		COPYCONFIGURATION=YES
+	;;
+	-h|--help)
+		HELP=YES
+	;;
+	-l|--coveralls)
+		COVERALLS=YES
+	;;
+	-q|--code-quality)
+		CODEQUALITY=YES
+	;;
+	-s|--scrutinizer)
+		SCRUTINIZER=YES
+	;;
+	-t|--travis)
+		TRAVIS=YES
+	;;
+	-u|--composer-update)
+		COMPOSERUPDATE=YES
+	;;
+	-v=*|--code-coverage=*)
+		CODECOVERAGE="${arg#*=}"
+		shift # past argument=value
+	;;
+	*)
+		# unknown option
+	;;
+esac
+shift # past argument or value
+done
+
+if [[ "$HELP" == "YES" ]]; then
+	echo "Usage: run-tests.sh [OPTION]..."
+	echo
+	echo "  -c, --copy-configuration    copy last API configuration before tests"
+	echo "  -l, --coveralls             upload code coverage report to Coveralls"
+	echo "                                requires -x"
+	echo "  -q, --code-quality          generate code quality report"
+	echo "  -s, --scrutinizer           upload code quality and code coverage reports to SCrutinizer"
+	echo "                                requires -q -x"
+	echo "  -t, --travis                shortcut for Travis, using: -u -c -v=xml -q -l -s"
+	echo "  -u, --composer-update       update Composer dependencies before tests"
+	echo "  -v, --code-coverage=TYPE    generate code coverage report:"
+	echo "                                TYPE=html for HTML output"
+	echo "                                TYPE=xml for Clover (XML) outpout"
+	echo "      --coverage-clover       equivalent to --code-coverage=xml"
+	echo "      --coverage-html         equivalent to --code-coverage=html"
+	echo
+	exit 0
 fi
 
-if [[ $@ == *"--composer-update"* ]]; then
+if [[ "$TRAVIS" == "YES" ]]; then
+	set @=$@"--composer-update --copy-configuration --coverage-clover --code-quality --coveralls --scrutinizer"
+	COMPOSERUPDATE=YES
+	COPYCONFIGURATION=YES
+	CODECOVERAGE=xml
+	CODEQUALITY=YES
+	COVERALLS=YES
+	SCRUTINIZER=YES
+fi
+
+if [[ "$COMPOSERUPDATE" == "YES" ]]; then
 	echox "${text_cyan}Update composer"
 	composer self-update
 	composer install --no-interaction --ignore-platform-reqs --prefer-source
 fi
 
-if [[ $@ == *"--copy-configuration"* ]]; then
+if [[ "$COPYCONFIGURATION" == "YES" ]]; then
 	echox "${text_cyan}Copy latest configuration files"
 	cp -v config/autoload/local.php.dist config/autoload/local.php
 	cp -v config/autoload/oauth2.local.php.dist config/autoload/oauth2.local.php
 fi
 
-if [[ $@ == *"--coverage-clover"* ]]; then
+if [[ "$CODECOVERAGE" == "xml" ]]; then
 	echox "${text_cyan}Run phpunit tests with Clover code coverage"
 	./vendor/bin/phpunit -c module/phpunit.xml --coverage-clover ./build/logs/coverage.xml
-elif [[ $@ == *"--coverage-html"* ]]; then
+elif [[ "$CODECOVERAGE" == "html" ]]; then
 	echox "${text_cyan}Run phpunit tests with HTML code coverage"
 	./vendor/bin/phpunit -c module/phpunit.xml --coverage-html ./build/logs/coverage
 else
@@ -33,17 +94,17 @@ else
 	./vendor/bin/phpunit -c module/phpunit.xml
 fi
 
-if [[ $@ == *"--code-quality"* ]]; then
+if [[ "$CODEQUALITY" == "YES" ]]; then
 	echox "${text_cyan}Check code quality"
 	./vendor/bin/phpcs --standard=PSR2 module/
 fi
 
-if [[ $@ == *"--coveralls"* ]]; then
+if [[ "$COVERALLS" == "YES" ]]; then
 	echox "${text_cyan}Send clover log to Coveralls"
 	./vendor/bin/coveralls -v -x ./build/logs/coverage.xml
 fi
 
-if [[ $@ == *"--scrutinizer"* ]]; then
+if [[ "$COVERALLS" == "SCRUTINIZER" ]]; then
 	echox "${text_cyan}Send clover log to Scrutinizer"
 	./vendor/bin/ocular code-coverage:upload --format=php-clover ./build/logs/coverage.xml
 fi

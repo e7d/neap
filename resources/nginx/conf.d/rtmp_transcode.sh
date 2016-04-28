161cda7a0a29ce64cf4e91e1867b7c7b2df68382
e7d <michael@e7d.io>
2016-04-28 23:38:56 +0200

    optimized rtmp configuration

--- resources/nginx/conf.d/rtmp_transcode.sh
+++ resources/nginx/conf.d/rtmp_transcode.sh
@@ -7,7 +7,7 @@ function on_die()
 
 trap 'on_die' TERM
 
-channel_id=$(curl http://api.neap.dev/rtmp/translate?stream_key=$1)
+channel_id=$(curl -s http://api.neap.dev/rtmp/translate?stream_key=$1)
 echo $(date +[%FT%TZ]) start transcode of \"$1\" to \"$channel_id\" >> /var/log/nginx/transcode.log
 ffmpeg -i rtmp://localhost/transcode/$1 -c copy -f flv rtmp://localhost/live/$channel_id 2> /var/log/ffmpeg/transcode-$1.log &
 wait

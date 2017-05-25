6cda710f9c5c2820de8e52133e1c3759288b3e57
e7d <michael@e7d.io>
2017-05-25 08:56:38 +0200

    fixed file descriptions

--- api/module/Application/src/Application/Database/DatabaseService.php
+++ api/module/Application/src/Application/Database/DatabaseService.php
@@ -2,9 +2,13 @@
 /**
  * Neap (http://neap.io/)
  *
- * @link      http://github.com/e7d/neap for the canonical source repository
- * @copyright Copyright (c) 2016 Michaël "e7d" Ferrand (http://github.com/e7d)
+ * @package   Neap
+ * @author    Michaël "e7d" Ferrand <michael@e7d.io>
+ * @copyright 2017 Michaël "e7d" Ferrand (http://github.com/e7d)
  * @license   https://github.com/e7d/neap/blob/master/LICENSE.txt The MIT License
+ * @link      https://github.com/e7d/neap
+ *
+ * PHP version 7.1
  */
 
 namespace Application\Database;
